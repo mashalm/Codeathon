1f89409e16b91e53d1f8413e1ce61e3eac51231d
Mashal Mannan <mashal.mannan@macys.com>
2015-07-22 15:26:00 -0400

    added pic when category selected

--- wear/src/main/java/com/example/b004235/macyswear/MenActivity.java
+++ wear/src/main/java/com/example/b004235/macyswear/MenActivity.java
@@ -34,7 +34,7 @@ public class MenActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_men);
     }
 //
 //    public MenActivity(Context ctx, FragmentManager fm) {
